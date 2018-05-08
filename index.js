12f1eebc8ab0ebc0ed777a6cbd2e235d95a145e5
Guilherme Souza <19gui94@gmail.com>
2018-05-07 23:05:50 -0300

    Add example code for bot #1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 'use strict'
+
+let Telegraf = require('telegraf'),
+	config = require('./config')
+
+let bot = new Telegraf(config.botToken)
+
+bot.start(ctx => ctx.reply('Welcome'))
+bot.help(ctx => ctx.reply('Send me a sticker'))
+bot.on('sticker', ctx => ctx.reply('👍'))
+bot.hears('hi', ctx => ctx.reply('Hey there'))
+bot.hears(/buy/i, ctx => ctx.reply('Buy-buy'))
+
+bot.startPolling()
