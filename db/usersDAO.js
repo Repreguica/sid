c95d20bd6342556b49783691d04b71c1058bffec
ddello32 <danieldroliveira@gmail.com>
2018-05-08 02:21:03 -0300

    Fixed getUsers

--- db/usersDAO.js
+++ db/usersDAO.js
@@ -4,7 +4,7 @@ const db = require('./firebase-db')
 const usersCollection = db.collection('users')
 
 module.exports = {
-	getUsers: () => usersCollection.get(),
+	getUsers: () => usersCollection.get().then(users => users.docs.map(doc => doc.data())),
 	getUser: id => usersCollection.doc(String(id)).get().then(doc => doc.data()),
 	setUser: (id, user) => usersCollection.doc(String(id)).set(user, {
 		merge: true
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,15 +7,17 @@ module.exports = (from, text) => {
 	if (!from) {
 		return Promise.reject(new Error('Usuario invalido'))
 	}
+	if (text === config.authPassword) {
+		return usersDAO.setUser(from.id, {
+			id: from.id,
+			firstName: from.first_name,
+			lastName: from.last_name || '',
+			username: from.username || ''
+		})
+	}
 	return usersDAO.getUser(from.id).then(user => {
 		if (user) {
 			return user
-		} else if (text === config.authPassword) {
-			return usersDAO.setUser(from.id, {
-				firstName: from.first_name,
-				lastName: from.last_name || '',
-				username: from.username || ''
-			})
 		}
 		return Promise.reject(new Error('Senha invalida'))
 	})
