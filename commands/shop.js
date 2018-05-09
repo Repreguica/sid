02f80bf4d864cb63f99aa325c67f15e53858fe62
ddello32 <danieldroliveira@gmail.com>
2018-05-09 20:52:10 -0300

    Changing messages instead of sending a new one in shop

--- commands/shop.js
+++ commands/shop.js
@@ -5,18 +5,18 @@ const bot = require('../bot').bot,
 	shopListDao = require('../db/shopListDAO')
 
 module.exports.setup = function () {
-	bot.command('/shop', sendShopList)
+	bot.command('/shop', ctx => sendShopList(ctx.replyWithMarkdown))
 	bot.command('/shopAdd', addItemToShopList)
 	bot.action(/REMOVE:.*/, removeItemFromShopList)
 }
 
-function sendShopList(ctx) {
+function sendShopList(callback) {
 	shopListDao.getList().then(items => {
 		if (!items || !items.length) {
-			ctx.reply('A LISTA TÁ VAZIA!')
+			callback('A LISTA TÁ VAZIA!')
 			return
 		}
-		ctx.replyWithMarkdown('LISTA DE COMPRAS (Clique para remover)', Markup.inlineKeyboard(
+		callback('LISTA DE COMPRAS (Clique para remover)', Markup.inlineKeyboard(
 			items.map(item => [Markup.callbackButton(item, 'REMOVE:' + item)])
 		).extra())
 	})
@@ -31,8 +31,7 @@ function removeItemFromShopList(ctx) {
 	let item = ctx.callbackQuery.data.substring('REMOVE:'.length)
 	shopListDao.removeItem(item)
 		.then(() => {
-			ctx.reply('Item removido com sucesso')
-			sendShopList(ctx)
+			sendShopList(ctx.editMessageText)
 		})
 		.catch(err => ctx.reply('Falho em remover item. Err: ' + err))
 }
